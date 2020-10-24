369250bddd57293122bcac58d1476f6d98385c18
Salty <achille.remael@yahoo.fr>
2020-10-24 14:11:58 +0200

    ajoute de la partie ratio de l'exo 4

--- Ex4.py
+++ Ex4.py
@@ -17,15 +17,28 @@ def recherche(txt):
 	for station in collec.find({"$text": {"$search": txt,"$caseSensitive":False}}):
 		pprint(station)
 
-recherche("Planetes")
+#recherche("Planetes")
 ##update
 
 
 #delete station
+#ne marche pas ??
+
 def remove_station(nom):
 	collec.deleteMany({"name":nom})
-remove_station("Planetes")
-recherche("Planetes")
+#remove_station("Planetes")
+#recherche("Planetes")
 #deactivate
 
 #give ratio
+
+'''c est un programe un peu nul, il part du principe que 
+les stations font toute 20 places et cherche les sizes plus petites
+que 20 * ratio /100 
+ c est le bon query mais faudra changer la bdd pour reelement faire l'exo'''
+def give_ration(ratio):
+	maxplaces=20
+	places=int(maxplaces*maxplaces/100)
+	for station in collec.find({"size": { "$lt": places}}):
+		pprint(station)
+give_ration(20)
